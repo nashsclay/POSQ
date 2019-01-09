928532769b25cde524dcb8fe1d941b6a2e2b9938
nashsclay <nashsclay@gmail.com>
2019-01-09 10:29:17 -0700

    Update POSQ_modified_install.sh

--- POSQ_modified_install.sh
+++ POSQ_modified_install.sh
@@ -3,7 +3,7 @@
 TMP_FOLDER=$(mktemp -d)
 CONFIG_FILE='posq.conf'
 CONFIGFOLDER='/root/.POSQ'
-COIN_DAEMON='poseqd'
+COIN_DAEMON='posqd'
 COIN_CLI='posq-cli'
 COIN_TX='posq-tx'
 COIN_PATH='/usr/local/bin/'

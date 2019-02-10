73f8a2212318f4f30473e68cddacd10cf4948f5f
nashsclay <nashsclay@gmail.com>
2019-02-10 15:06:14 -0700

    Update POSQ_update_mn.sh

--- POSQ_update_mn.sh
+++ POSQ_update_mn.sh
@@ -1,5 +1,5 @@
 #/bin/bash
-systemctl stop poseidonquark.service
+systemctl stop posq.service
 clear
 cd ~
 
@@ -12,12 +12,9 @@ echo "Script may appear frozen for a few seconds. This is normal."
 wget https://github.com/nashsclay/POSQ/releases/download/v2.1.4/posq_mn.zip
 sudo rm posqd posq-cli posq-tx
 unzip posq_mn.zip >/dev/null 2>&1
-sudo mv -f poseidon-cli /usr/local/bin
-sudo mv -f poseidond /usr/local/bin
-sudo mv -f poseidon-tx /usr/local/bin
 sudo rm posq_mn.zip
 echo "! Restarting POSQ Masternode !"
-cd ~/.poseidon
+cd ~/.posq
 sudo rm ~/.posq/db.log ~/.posq/debug.log ~/.posq/posqd.pid
 sudo rm -R ~/.posq/backups/
 sudo rm -R ~/.posq/blocks/
@@ -26,5 +23,5 @@ sudo rm -R ~/.posq/database/
 sudo rm -R ~/.posq/sporks/
 sudo rm -R ~/.posq/zerocoin/
 sudo rm ~/.posq/budget.dat ~/.posq/db.log ~/.posq/debug.log ~/.posq/fee_estimates.dat ~/.posq/mncache.dat ~/.posq/mnpayments.dat ~/.posq/peers.dat
-systemctl start poseidonquark.service
+systemctl start posq.service
 echo "Masternode restarted. Any errors about files not existing is ok."
